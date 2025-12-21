cbfc3bd6929d65a6308aef6989ea57d3b8e6e574
Abdo <abdo.el.kabli12@gmail.com>
2025-12-21 21:28:22 +0100

    Addind the add card functionality version 2.0

--- Card_add.php
+++ Card_add.php
@@ -4,38 +4,54 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tracker";
+$dbname = "Smart_wallet";
 
 $sql_con = new mysqli($servername, $username, $password, $dbname);
 if ($sql_con->connect_error) {
   die("Connection failed: " . $sql_con->connect_error);
 }
 
+$colors = [
+  'indigo-purple',
+  'green-teal',
+  'orange-red',
+  'blue-cyan',
+  'pink-purple'
+];
 
-$user_name = $_POST['username'];
+// pick random color for new card
 
-//gets you the last id added in sql (aka the PRIMARY KEY of the user)
-$user_id = mysqli_insert_id($sql_con);
+$stmt = $sql_con->prepare("SELECT id FROM users WHERE Email = ?");
+$stmt->bind_param("s", $_SESSION['user']);
+$stmt->execute();
+$stmt->bind_result($user_id);
+$stmt->fetch();
+$stmt->close();
 
-  $Card_number = $_POST['card-number'];
-  $Card_name = $_POST['card-name'];
-  $Card_date = $_POST['expire-date'];
-  $Card_cvv =  $_POST['cvv'];
+$_SESSION['user_id'] = $user_id;
 
-  if(isset($_POST['Visa'])) {
-    $card_type = $_POST['Visa'];
-  }
-  if(isset($_POST['MasterCard'])) {
-    $card_type = $_POST['MasterCard'];
-  }
+if (!$user_id) {
+  die("User not found");
+}
 
+if(isset($_POST['addCard'])){
+  
+  $card_color = $colors[array_rand($colors)];
+  $Card_number = trim($_POST['card-number']);
+  $Card_name   = trim($_POST['card-name']);
+  $Card_date   = $_POST['expire-date']; // (not stored yet)
+  $Card_cvv    = trim($_POST['cvv']);
 
+  $card_type = "Visa";
 
+  $stmt1 = $sql_con->prepare("INSERT INTO cards(user_id, Card_type, serial_num, CVV, placeholder, card_color) VALUES(?, ? ,?, ?, ?, ?)");
+  $stmt1->bind_param("isssss", $user_id, $card_type, $Card_number, $Card_cvv, $Card_name, $card_color);  
+    $stmt1->execute();
+      $stmt1->close();
+}
 
+  $sql_con->close();
 
-  $stmt = $sql_con->prepare("INSERT INTO cards(user_id, Card_type, serial_num, CVV, placeholder) VALUES(?, ?, ? ,?, ?)");
-  $stmt->bind_param("isiis", $usrr_id, $card_type, )
-
-
-
+header("Location: test.php");
+exit;
 ?>
