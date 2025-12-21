cb99607523ddf8f7ab83d80d6281d4225fada8f8
Abdo <abdo.el.kabli12@gmail.com>
2025-12-21 21:28:52 +0100

    Retouching the Income functionality

--- Income.php
+++ Income.php
@@ -3,8 +3,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tracker";
+$dbname = "Smart_Wallet";
 
+session_start();
 
 $sql_con = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,6 +13,11 @@ if (isset($_POST['Incomes']) && isset($_POST['descreption'])){
   $Incomes = trim($_POST['Incomes']);
   $Desc = trim($_POST['descreption']);
   $Date = $_POST['date'];
+  if (!isset($_POST['card_id'])) {
+  die("No card selected");
+}
+
+$card_id = intval($_POST['card_id']);
         
     if ($Incomes !== '') {
 
@@ -22,15 +28,19 @@ if (isset($_POST['Incomes']) && isset($_POST['descreption'])){
       }
 
 
+
             
       if($Date != '') {
-        $stmt = $sql_con->prepare("INSERT INTO income_tracker(Income, descr, Date) VALUES(?, ?, ?)");
-        $stmt->bind_param("iss", $Incomes, $Desc, $Date);
+        $stmt = $sql_con->prepare("INSERT INTO incomes_tracker(card_id, Incomes, description, Date) VALUES(?, ?, ?, ?)");
+        $stmt->bind_param("iiss", $card_id ,$Incomes, $Desc, $Date);
       } else {
-            $stmt = $sql_con->prepare("INSERT INTO income_tracker (Income, descr, Date) VALUES (?, ?, CURRENT_DATE())");
-            $stmt->bind_param("is", $Incomes, $Desc);
+            $stmt = $sql_con->prepare("INSERT INTO incomes_tracker (card_id, Incomes, description, Date) VALUES (?, ?, ?, CURRENT_DATE())");
+            $stmt->bind_param("iis", $card_id, $Incomes, $Desc);
+
+            
 
       }
+
       $stmt->execute();
       $stmt->close();
     }
@@ -46,12 +56,12 @@ if (isset($_POST['Incomes']) && isset($_POST['descreption'])){
         if($EIncomes !== '') {
         if (!empty($EDate)) {
             // Update including date
-            $stmt = $sql_con->prepare("UPDATE income_tracker SET Income = ?, descr = ?, Date = ? WHERE id = ?");
+            $stmt = $sql_con->prepare("UPDATE incomes_tracker SET Incomes = ?, description = ?, Date = ? WHERE inc_id = ?");
             $stmt->bind_param("issi", $EIncomes, $EDesc, $EDate, $id);
 
         } else {
             // Update without date change
-            $stmt = $sql_con->prepare("UPDATE income_tracker SET Income = ?, descr = ? WHERE id = ?");
+            $stmt = $sql_con->prepare("UPDATE incomes_tracker SET Incomes = ?, description = ? WHERE inc_id = ?");
             $stmt->bind_param("isi", $EIncomes, $EDesc, $id);
         }
           $stmt->execute();
@@ -61,11 +71,12 @@ if (isset($_POST['Incomes']) && isset($_POST['descreption'])){
 
       if (isset($_POST['Did'])){
         $Did = $_POST['Did'];
-        $stmt = $sql_con->prepare("DELETE FROM income_tracker WHERE id = ?");
+        $stmt = $sql_con->prepare("DELETE FROM incomes_tracker WHERE inc_id = ?");
         $stmt->bind_param("i", $Did);
         $stmt->execute();
         $stmt->close();
       }
+
 
 
     $sql_con->close();
